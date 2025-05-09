89093e8db9474b54fde109f49c83b3334883d533
Egil Eskilsson <egil@egileskilsson.se>
2025-05-09 11:11:55 +0200

    feat: set up Vue with routing and base styling
    Add Vue component files with VueRouter integration
    Create SCSS directory with foundational partials (reset, colors, base, container)
    Initialize basic application structure

---
+++ client/src/views/AdminView.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <div class="books">
+    <h1>This is a book list page</h1>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
---
+++ client/src/views/LoginView.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <div class="login">
+    <h1>Register page</h1>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
