0c5510182e49b47314b3a4855f4e5050ba08a9c9
David Brunni <davidbrunni@hotmail.com>
2025-05-12 17:11:25 +0200

    auth-index-install
    auth.js fil samt ändringar i index.js

--- client/src/App.vue
+++ client/src/App.vue
@@ -8,6 +8,20 @@
 
 <template>
 
+  <header>
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/register">Register page</RouterLink>
+        <RouterLink to="/login">Login page</RouterLink>
+        <RouterLink to="/admin">Admin panel</RouterLink>
+        <RouterLink to="/books">Books page</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+
   <SvgSymbols />
   <MainHeader />
   <RouterView />
