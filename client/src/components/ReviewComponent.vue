3af9464a6d5859ec84dced736188f4c4de355739
AgnesWilson <agnes.wilson97@hotmail.com>
2025-05-13 21:38:33 +0200

    feat: added a review component
    added a review component to GET the reviews connected to a specific book.

---
+++ client/src/components/ReviewComponent.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps({
+  reviews: {
+    type: Array,
+    required: true
+  }
+});
+</script>
+
+<template>
+  <div v-if="reviews.length > 0">
+    <ul>
+      <li v-for="review in reviews" :key="review._id" class="review">
+        <p>{{ review.title }}</p>
+        <p>{{ review.content }}</p>
+        <p>{{ review.rating }}</p>
+        <p>{{ review.name }}</p>
+        <p>{{ new Date(review.created_at).toLocaleDateString() }}</p>
+      </li>
+    </ul>
+  </div>
+
+  <div v-else>
+    <p>Inga recensioner ännu.</p>
+  </div>
+</template>
