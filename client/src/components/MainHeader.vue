6948ee72727870f51dfc56dd7254bf2e5194a17b
Egil Eskilsson <egil@egileskilsson.se>
2025-05-14 15:29:10 +0200

    feat: add logout link

--- client/src/components/MainHeader.vue
+++ client/src/components/MainHeader.vue
@@ -14,8 +14,26 @@
         <IconSvg name="brand" />
       </RouterLink>
 
-      <nav class="nav">
+      <!-- <input type="checkbox" id="menu-active" class="menu-toggle-input" aria-hidden="true"/>
+
+      <div class="menu-toggle">
+        <label
+          for="menu-active"
+          class="menu-toggle__label"
+          aria-expanded="false"
+          aria-controls="main-navigation"
+          aria-label="Toggle menu"
+          tabindex="0">
+          <span class="menu-toggle__icon" aria-hidden="true">
+            <span class="menu-toggle__line"></span>
+          </span>
+          <span class="visually-hidden">Menu</span>
+        </label>
+      </div> -->
+
+      <nav id="main-navigation" class="nav">
         <ul class="nav__menu">
+
           <!-- Public links always visible -->
           <li class="nav__item">
             <RouterLink to="/" class="nav__link">Hem</RouterLink>
@@ -24,7 +42,7 @@
             <RouterLink to="/books" class="nav__link">Böcker</RouterLink>
           </li>
 
-          <!-- Only when loggen in and 'is_admin' is true -->
+          <!-- Only when loggen in -->
           <template v-if="isLoggedIn">
 
             <!-- Links visible only for admin users -->
@@ -32,11 +50,12 @@
               <li class="nav__item">
                 <RouterLink to="/admin" class="nav__link">Admin</RouterLink>
               </li>
-              <li class="nav__item">
-                <!-- Button to logout -->
-                <button>Logga ut</button>
-              </li>
             </template>
+
+            <li class="nav__item">
+              <!-- Button to logout -->
+              <a href="#">Logga ut</a>
+            </li>
 
           </template>
 
@@ -50,8 +69,6 @@
             </li>
 
           </template>
-
-
 
         </ul>
 
@@ -76,6 +93,7 @@
   ;
 
   &__inner {
+    position: relative;
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -98,6 +116,8 @@
 
 }
 
+
+/// Legacy
 .nav {
   width: 100%;
   display: flex;
@@ -129,14 +149,122 @@
     margin-inline-start: 1rem;
   }
 }
+/// --------
 
 
-@media screen and (min-width: 992px) {
+// .menu-toggle {
 
-  .nav {
+//   display: inline-block;
+//   width: 3rem;
+//   height: 3rem;
+//   background-color: red;
 
-  }
 
-}
+//   &__label {
+//     cursor: pointer;
+//     width: 100%;
+//     height: 100%;
+//     display: block;
+//   }
+
+//   &__label:hover &__line {
+//     background-color: var(--color-primary-tint);
+//   }
+
+//   &__icon {
+//     position: relative;
+//     width: 100%;
+//     height: 100%;
+//     display: flex;
+//     flex-direction: column;
+//     justify-content: center;
+//     align-items: center;
+//   }
+
+//   &__line {
+//     position: absolute;
+//     top: 50%;
+//     left: 50%;
+//     transform: translate(-50%, -50%);
+//     width: 100%;
+//     height: .25rem;
+//     background-color: var(--color-primary);
+//   }
+// }
+
+// .nav {
+//   display: block;
+//   position: absolute;
+//   top: 6.5rem;
+//   left: 0;
+//   width: 100%;
+//   height: calc(100dvh - 6.5rem);
+
+//   &__menu {
+//     list-style: none;
+//     display: flex;
+//     flex-flow: row nowrap;
+//     align-items: center;
+//   }
+
+//   &__item {
+//     font-size: var(--font-size-400);
+//   }
+
+//   &__link {
+//     padding: .5em 1em;
+//     text-decoration: none;
+//     color: var(--color-primary);
+//   }
+
+//   &__link:hover {
+//     color: var(--color-accent);
+//   }
+
+//   &__contact {
+//     margin-inline-start: 1rem;
+//   }
+// }
+
+
+// @media screen and (min-width: 992px) {
+
+//   .menu-toggle {
+//     display: none;
+//   }
+
+//   .nav {
+//     width: 100%;
+//     display: flex;
+//     justify-content: flex-end;
+//     align-items: center;
+
+//     &__menu {
+//       list-style: none;
+//       display: flex;
+//       flex-flow: row nowrap;
+//       align-items: center;
+//     }
+
+//     &__item {
+//       font-size: var(--font-size-400);
+//     }
+
+//     &__link {
+//       padding: .5em 1em;
+//       text-decoration: none;
+//       color: var(--color-primary);
+//     }
+
+//     &__link:hover {
+//       color: var(--color-accent);
+//     }
+
+//     &__contact {
+//       margin-inline-start: 1rem;
+//     }
+//   }
+
+// }
 
 </style>
