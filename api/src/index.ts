0ab0fd0b7ca560f8808ae037e4d163bd0160edb9
AgnesWilson <agnes.wilson97@hotmail.com>
2025-05-16 15:11:02 +0200

    fix: updtae cors and cookie-settings
    - update cors to new varcel adress
    - change the cookie acess to accept split browsers

--- api/src/index.ts
+++ api/src/index.ts
@@ -15,7 +15,8 @@ const app = express();
 // Midlewares
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  //origin: 'http://localhost:5173', 
+  origin: 'https://group-assignment-client.vercel.app',
   credentials: true   
 }));
 app.use(cookieParser());
